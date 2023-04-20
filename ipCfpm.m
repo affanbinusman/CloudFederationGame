b5ac69ee162983a244b23e5a27b8331a2df36c42
Tabsheer Ali Askari <45563564+Tabsheer@users.noreply.github.com>
2023-04-20 02:00:15 -0700

    Update ipCfpm.m

--- ipCfpm.m
+++ ipCfpm.m
@@ -14,26 +14,26 @@
 %  2., 3.75, 0.48, 0.24;
 %  4., 7.5, 0.98, 0.48;
 %  8., 15., 1.99, 0.96];
-% userRequest = [1 100 2 50];
+% userRequest = [100 50 200 50];
 % ipCfpm1(availableResourcesInFed, costsOfCPsInFed, vmInfo, userRequest)
-% 
 
-function X = ipCfpm(availableResourcesInFed, costsOfCPsInFed, vmInfo, userRequest)
-    m = size(availableResourcesInFed,1)    %number of federations
-    n = size(vmInfo,1)     %number of virtual machine types 
-    
-    p = vmInfo(:,4) %pricelist
-    c = costsOfCPsInFed'    %costlist
-    
-    N = availableResourcesInFed(:,1)
-    M = availableResourcesInFed(:,2)
-    S = availableResourcesInFed(:,3)
 
-    r = userRequest
+function x, profit = ipCfpm(availableResourcesInFed, costsOfCPsInFed, vmInfo, userRequest)
+    m = size(availableResourcesInFed,1);    %number of federations
+    n = size(vmInfo,1);     %number of virtual machine types 
     
-    w_c = vmInfo(:,1)    
-    w_m = vmInfo(:,2)    
-    w_s = vmInfo(:,3)    
+    p = vmInfo(:,4); %pricelist
+    c = costsOfCPsInFed';    %costlist
+    
+    N = availableResourcesInFed(:,1);
+    M = availableResourcesInFed(:,2);
+    S = availableResourcesInFed(:,3);
+
+    r = userRequest;
+    
+    w_c = vmInfo(:,1);    
+    w_m = vmInfo(:,2);    
+    w_s = vmInfo(:,3);    
     
 
     %% cvx implementation: has bugs
@@ -74,35 +74,52 @@ function X = ipCfpm(availableResourcesInFed, costsOfCPsInFed, vmInfo, userReques
     %% YALMIP implementation
     x = intvar(m,n);
 
-    cons = [];
+    cons = []; %constraints
+
+    % (2)
     for i = 1:m
         cons = [cons, x(i,:)*w_c<=N(i)];
     end
+    % (3)
     for i = 1:m
         cons = [cons, x(i,:)*w_m<=M(i)];
     end
+
+    % (4)
     for i = 1:m
         cons = [cons, x(i,:)*w_s<=S(i)];
     end
 
+    % (5)
     for j = 1:n
         cons = [cons, sum(x(:,j))==r(:,j)];
     end
-
+    
+    % (6)
     for i=1:m 
         cons = [cons, sum(x(i,:))>=1];
     end
+
+    % (7)
     for i = 1:m
         for j = 1:n
             cons = [cons, x(i,j)>=0];
         end
     end
+
+    % objective (1)
     objective = 0;
     for i = 1:m
         objective = objective + x(i,:)*(p-c(:,i));
     end
-    opt=sdpsettings('solver','gurobi','verbose',0);
-    optimize(cons,-objective,opt)
 
-    X = value(x);
+    % solver settings
+    opt=sdpsettings('solver','gurobi','verbose',0);
+    optimize(cons,-objective,opt);
+
+    % federation profit
+    profit = value(objective);
+
+    %V Ms required from each CP
+    x = value(x);
 end
